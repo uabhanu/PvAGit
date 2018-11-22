07bbee9a21b1943eefd24c98d49ce27eb5a9468e
Agasthyeaswar Bhanu Upadhyayula <uabhanu@gmail.com>
2018-11-22 22:56:11 +0000

    Project Upgrade & ECS
    1. Upgraded to Unity 2018.2.17f1
    2. ECS WIP

---
+++ PlantsVsAnimalsAndroidECS/Assets/Scripts/ECS/CleanupFiringSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+public class CleanupFiringSystem : JobComponentSystem
+{
+    struct CleanupFiringJob : IJobParallelFor
+    {
+        [ReadOnly] public EntityArray m_entities;
+        public EntityCommandBuffer.Concurrent m_entityCommandBuffer;
+
+        public void Execute(int i)
+        {
+            m_entityCommandBuffer.RemoveComponent<Firing>(m_entities[i]);
+        }
+    }
+
+    struct Data
+    {
+        public readonly int m_length;
+        public EntityArray m_entities;
+        public ComponentDataArray<Firing> m_firings;
+    }
+
+    [Inject] CleanupFiringBarrier _barrier;
+    [Inject] Data _data;
+}
+
+public class CleanupFiringBarrier : BarrierSystem
+{
+
+}
---
+++ PlantsVsAnimalsAndroidECS/Assets/Scripts/ECS/PlayerShootingSystem.cs
@@ -0,0 +1,53 @@
+﻿using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+
+public class PlayerShootingSystem : JobComponentSystem
+{
+
+    public class PlayerShootingBarrier : BarrierSystem
+    {
+
+    }
+
+    struct Data
+    {
+        public readonly int m_length;
+        public EntityArray m_entities;
+        public ComponentDataArray<Weapon> m_weapons;
+        public SubtractiveComponent<Firing> m_firingToSubtract;
+    }
+
+    [Inject] Data _data;
+    [Inject] PlayerShootingBarrier _playerShootingBarrier;
+
+    struct PlayerShootingJob : IJobParallelFor
+    {
+        public bool m_bIsFiring;
+        public float m_currentTime;
+        [ReadOnly] public EntityArray m_entityArray;
+        public EntityCommandBuffer.Concurrent m_entityCommandBuffer; // Command Buffers are used to queue up jobs so Unity can decide the best time to use them and Concurrent in particular are used for parallel jobs 
+
+
+        public void Execute(int i)
+        {
+            if (!m_bIsFiring)
+            {
+                return;
+            }
+
+            m_entityCommandBuffer.AddComponent(m_entityArray[i], new Firing{m_firedAt = m_currentTime});
+        }
+    }
+
+    protected override JobHandle OnUpdate(JobHandle inputDeps)
+    {
+        return new PlayerShootingJob
+        {
+            m_currentTime = Time.time,
+            m_entityArray = _data.m_entities,
+            m_entityCommandBuffer = _playerShootingBarrier.CreateCommandBuffer()
+            //m_bIsFiring = This part later
+        }.Schedule(_data.m_length, 64, inputDeps);
+    }
+}
